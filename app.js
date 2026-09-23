3daa4ddd90a5c01810d08d588a0f5ab15b2ddf53
1MasonMuch <masonm126@nycstudents.net>
2026-09-23 14:02:00 -0400

    1

---
+++ app.js
@@ -0,0 +1,8 @@
+const students = ["Charlie", "Olivia", "Rodney", "Bill", "London"];
+
+function printAll(list) {
+  for (let i = 0; i < list.length; i++) {
+    console.log(list[i]);
+  }
+}
+printAll(students);
